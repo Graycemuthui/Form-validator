883988542f63587151db78a81a63fe748033c12c
Graycemuthui <graycemuthui20@gmail.com>
2022-06-07 01:15:30 +0300

    FIx linters

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 // Manipulate the form with DOM
-const form = document.getElementById("form");
-const username = document.getElementById("username");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const password2 = document.getElementById("password2");
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
 // Show input error message and outline
 const showError = (input, message) => {
   const formControl = input.parentElement;
-  formControl.className = "form-control error";
-  const small = formControl.querySelector("small");
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
   small.innerText = message;
 };
 
 // Show success outline
 const showSuccess = (input) => {
   const formControl = input.parentElement;
-  formControl.className = "form-control success";
+  formControl.className = 'form-control success';
 };
 
 // Check email is valid and lowercase
 function checkEmail(input) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (re.test(input.value.trim())) {
     showSuccess(input);
   } else {
-    showError(input, "Email is not valid");
+    showError(input, 'Email is not valid');
   }
 }
+// Get fieldname and change the first letter to uppercase
+const getFieldName = (input) => input.id.charAt(0).toUpperCase() + input.id.slice(1);
 
 // Check required fields
 const checkRequired = (inputArr) => {
-  inputArr.forEach(function (input) {
-    if (input.value.trim() === "") {
+  inputArr.forEach((input) => {
+    if (input.value.trim() === '') {
       showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
@@ -46,12 +47,12 @@ const checkLength = (input, min, max) => {
   if (input.value.length < min) {
     showError(
       input,
-      `${getFieldName(input)} must be atleast ${min} characters`
+      `${getFieldName(input)} must be atleast ${min} characters`,
     );
   } else if (input.value.length > max) {
     showError(
       input,
-      `${getFieldName(input)} must be less than ${max} characters`
+      `${getFieldName(input)} must be less than ${max} characters`,
     );
   } else {
     showSuccess(input);
@@ -61,17 +62,12 @@ const checkLength = (input, min, max) => {
 // Check passwords match
 const checkPasswordsmatch = (input1, input2) => {
   if (input1.value !== input2.value) {
-    showError(input2, "Passwords do not match");
+    showError(input2, 'Passwords do not match');
   }
 };
 
-// Get fieldname and change the first letter to uppercase
-const getFieldName = (input) => {
-  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-};
-
 // Add event listener to form
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   checkRequired([username, email, password, password2]);
